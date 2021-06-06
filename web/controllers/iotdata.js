c98685b8ca99c15e35cc78b4e4de5ca62fba387d
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2021-06-06 20:22:18 +0700

    use update not save

--- web/controllers/iotdata.js
+++ web/controllers/iotdata.js
@@ -2,7 +2,6 @@ const iotdata = require("../models/iotdata");
 const Iotdata = require("../models/iotdata");
 
 exports.data_dummy = async (req,res,next)=>{
-    const adata = new Iotdata({devid: "5510ecg", datastring: "55;65;72;33;44"});
-    await adata.save().then(()=>console.log("new data added"));
+    await Iotdata.update({devid: "5510ecg", datastring: "55;65;72;33;44"},{ upsert: true });
     res.send("new data added");
 };
