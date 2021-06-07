71e80469382f17b8ed7a2907b38d28d62ad81a2e
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2021-06-07 13:42:21 +0700

    api GET test

--- web/controllers/iotdata.js
+++ web/controllers/iotdata.js
@@ -1,10 +1,13 @@
 const Iotdata = require("../models/iotdata");
 
+//URL: http://urldomainip/iotdata?devid=xxx&data=yyy
+
 exports.data_dummy = async (req,res,next)=>{
     const reqdevid = req.query.devid;
+    const reqdata = req.query.data;
     await Iotdata.create({
         devid: reqdevid,
-        datastring: "10;24;23;45;67;56"
+        datastring: reqdata
     }).then(() => console.log("new data added"));
     res.send("new data added");
 };
