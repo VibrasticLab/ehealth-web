28d874ac07659ecdf21b0270ae6f6372da96ddb4
Pras Kiven <56885763+arkiven4@users.noreply.github.com>
2021-12-19 22:09:39 +0700

    Update send_post.py

--- latex/test_python/send_post.py
+++ latex/test_python/send_post.py
@@ -1,7 +1,7 @@
 import requests
 
-files = {'upload_file': open('coug.mp3','rb')}
 #files = {'upload_file': open('coug.mp3','rb'), 'upload_file2': open('coug.mp3','rb')} # Contoh Multifile
-values = {'DB': 'photcat', 'OUT': 'csv', 'SHORT': 'short'}
+files = {'file_batuk': open('coug.mp3','rb')}
+values = {'nama': 'pasien', 'gender': 'unknown', 'umur': 0}
 
-r = requests.post("https://webhook.site/d717402a-4a8a-4c81-a6d2-af0f6bef2085", files=files, data=values)
+r = requests.post("http://10.124.5.198/api/device/sendData/303", files=files, data=values)
