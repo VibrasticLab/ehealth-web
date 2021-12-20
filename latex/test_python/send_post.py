91daf330286ee395b66dd263f446f306774090ed
Pras Kiven <56885763+arkiven4@users.noreply.github.com>
2021-12-20 09:23:46 +0700

    Update send_post.py

--- latex/test_python/send_post.py
+++ latex/test_python/send_post.py
@@ -4,4 +4,4 @@ import requests
 files = {'file_batuk': open('coug.mp3','rb')}
 values = {'nama': 'pasien', 'gender': 'unknown', 'umur': 0}
 
-r = requests.post("http://10.124.5.198/api/device/sendData/303", files=files, data=values)
+r = requests.post("10.124.5.198/api/device/sendData/303", files=files, data=values)
